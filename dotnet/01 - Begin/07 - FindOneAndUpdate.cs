7aa2644009f5a19cb00b88ffade9893f464454fb
Remi THOMAS <remi@xtware.com>
2022-05-17 11:09:12 +0200

    Typo fix

--- dotnet/01 - Begin/07 - FindOneAndUpdate.cs
+++ dotnet/01 - Begin/07 - FindOneAndUpdate.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class BeingFindOneAndUpdate : BaseClass
+public class BeginFindOneAndUpdate : BaseClass
 {
 
     
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -5,6 +5,6 @@ await (new BeginSimple()).RunAsync();
 await (new BeginSerialization()).RunAsync();
 await (new BeginAttributes()).RunAsync();
 await (new BeginIndex()).RunAsync();
-await (new BeingFindOneAndUpdate()).RunAsync();
+await (new BeginFindOneAndUpdate()).RunAsync();
 
 await (new DynamicQuery()).RunAsync();
